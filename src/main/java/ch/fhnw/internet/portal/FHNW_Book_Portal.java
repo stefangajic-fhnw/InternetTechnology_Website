912d63cc220a0c426af70d804c325dfccb0cd37e
stefangajic-fhnw <55506080+stefangajic-fhnw@users.noreply.github.com>
2020-12-17 16:13:07 +0100

    - DB connection not working

---
+++ src/main/java/ch/fhnw/internet/portal/FHNW_Book_Portal.java
@@ -0,0 +1,11 @@
+package ch.fhnw.internet.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FHNW_Book_Portal{
+    public static void main(String[] args){
+        SpringApplication.run(FHNW_Book_Portal.class, args);
+    }
+}
